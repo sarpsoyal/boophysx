9da32eaecd2fa6d12c3ace575ef78ef246af3705
Sarp Soyal <sarp.soyal@durasoftware.com>
2018-01-01 12:59:12 -0500

    Added proper collision handling & Minor tweaks

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-/* KNOWN ISSUES:
-    -> No collision handling. */
-
 function start(){
   mainArea.init();
   comp1 = new component(muts.randColor(),
@@ -33,24 +30,14 @@ function start(){
                         0,
                         0,
                         true);
-  // ball = new component('black', 30, 30, 0, mainArea.canvas.height/2, 5, 0, 0, 0, true);
-  // thing = new component('blue', 50, 50, mainArea.canvas.width/2 + 100, mainArea.canvas.height/2, 0, 0, 0, 0, true);
-  floor = new component('black', mainArea.canvas.width, 5, 0, mainArea.canvas.height-5, 0, 0, 0, 0, false);
-}
 
-//repeat this every 20ms
-function updatemainArea(){
-  mainArea.clear();
-  for (cmpnt in components){
-    components[cmpnt].newPos();
-    components[cmpnt].update();
-  }
+  floor = new component('black', mainArea.canvas.width, 5, 0, mainArea.canvas.height-5, 0, 0, 0, 0, false);
 }
 
 //Component array
 components = [];
 
-//Component Object
+//Component object constructor
 function component(color, width, height, x, y, velX, velY, accX, accY, grav) {
     this.width = width;
     this.height = height;
@@ -73,14 +60,25 @@ function component(color, width, height, x, y, velX, velY, accX, accY, grav) {
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 
+    //checks wheter two obejcts collide
+    this.willCollide = function(other) {
+      return Math.max(this.x + this.accX + this.velX, this.x + this.accX + this.velX + this.width) >= Math.min(other.x, other.x + other.width) &&
+             Math.min(this.x + this.accX + this.velX, this.x + this.accX + this.velX + this.width) <= Math.max(other.x, other.x + other.width) &&
+             Math.max(this.y + this.accY + this.velY, this.y + this.accY + this.velY + this.height) >= Math.min(other.y, other.y + other.height) &&
+             Math.min(this.y + this.accY + this.velY, this.y + this.accY + this.velY + this.height) <= Math.max(other.y, other.y + other.height)
+    }
+
+    //check for a collision in next frame
+    this.collides = function(other) {
+      return Math.max(this.x, this.x + this.width) >= Math.min(other.x, other.x + other.width) &&
+             Math.min(this.x, this.x + this.width) <= Math.max(other.x, other.x + other.width) &&
+             Math.max(this.y, this.y + this.height) >= Math.min(other.y, other.y + other.height) &&
+             Math.min(this.y, this.y + this.height) <= Math.max(other.y, other.y + other.height)
+    }
     this.newPos = function() { //calc future position
       //Handle collision
       for (obj in components){
-        if (components[obj] != this && doesCollide(this, components[obj])){
-          //components[obj].velX = 0;
-          //components[obj].velY = 0;
-          //components[obj].accX = 0;
-          //components[obj].accY = 0;
+        if (components[obj] != this && this.willCollide(components[obj])){
           this.velX = 0;
           this.velY = 0;
           this.accX = 0;
@@ -88,7 +86,7 @@ function component(color, width, height, x, y, velX, velY, accX, accY, grav) {
         }
       }
       //handle velocity&acceleration
-      if (this.grav && !doesCollide(this, floor)){
+      if (this.grav && !this.collides(floor)){
         this.accY += 0.01; //ITS GRAVITY BOI
       }
       this.velX += this.accX;
@@ -97,13 +95,7 @@ function component(color, width, height, x, y, velX, velY, accX, accY, grav) {
       this.y += this.velY;
     }
 }
-//checks wheter two obejcts collide
-function doesCollide(obj1, obj2){
-  return Math.max(obj1.x, obj1.x + obj1.width) >= Math.min(obj2.x, obj2.x + obj2.width) &&
-         Math.min(obj1.x, obj1.x + obj1.width) <= Math.max(obj2.x, obj2.x + obj2.width) &&
-         Math.max(obj1.y, obj1.y + obj1.height) >= Math.min(obj2.y, obj2.y + obj2.height) &&
-         Math.min(obj1.y, obj1.y + obj1.height) <= Math.max(obj2.y, obj2.y + obj2.height)
-}
+
 //game canvas object
 var mainArea = {
     canvas : document.getElementById("mainCanvas"),
@@ -112,10 +104,18 @@ var mainArea = {
         this.canvas.height = 400;
         this.ctx = this.canvas.getContext("2d");
         //Every 20ms, update frame
-        this.interval = setInterval(updatemainArea, 20);
+        this.interval = setInterval(this.update, 20);
     },
     clear : function() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    //repeat this every 20ms
+    update : function() {
+      mainArea.clear();
+      for (cmpnt in components){
+        components[cmpnt].newPos();
+        components[cmpnt].update();
+      }
     }
 }
 
